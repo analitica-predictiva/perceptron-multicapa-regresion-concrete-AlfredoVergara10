32c498355713afbb3cad740b05cb8e4d45d9ef06
AlfredoVergara10 <112211579+AlfredoVergara10@users.noreply.github.com>
2022-12-05 15:54:00 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -19,13 +19,13 @@ def pregunta_01():
     df = pd.read_csv("concrete.csv")
 
     # Asigne la columna `strength` a la variable `y`.
-    y = df['strength'] 
+    y = df["strength"] 
 
     # Asigne una copia del dataframe `df` a la variable `X`.
     X = df.copy() 
 
     # Remueva la columna `strength` del DataFrame `X`.
-    X.drop('strength', axis=1, inplace=True) 
+    X.drop("strength", axis=1, inplace=True) 
 
     # Retorne `X` y `y`
     return X, y
